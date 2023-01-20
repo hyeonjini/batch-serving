e65cc1278df29dd65fba792d2908f374fdacdec0
hyeonjini <choihj94@huray.net>
2023-01-20 12:46:27 +0900

    [ADD] classifier

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,9 +1,16 @@
-from collections import abc
-
+from src.loader.loader import ModelLoader
+import torch
 
 class Classifier:
-    def __init__(self, model_path:str) -> None:
-        pass
+    def __init__(
+        self,
+        model_path:str,
+        loader: ModelLoader,
+    ) -> None:
+
+        self.model_path: str = model_path
+        self.loader: ModelLoader = loader
+        self.model = self.loader.get_model()
 
     def inference(self):
         return NotImplementedError()
@@ -16,14 +23,23 @@ class ImageClassifier(Classifier):
     def __init__(self) -> None:
         pass
 
-    def inference(self):
-        return super().inference()
+    def inference(self, data):
+        outputs = {}
+        with torch.no_grad():
+            print("inference code")
+
+        return outputs
 
 class Textlassifier(Classifier):
     def __init__(self) -> None:
         super().__init__()
     
-    def inference(self):
+    def inference(self, data):
+
+        with torch.no_grad():
+
+            print("inference code")
+
         return super().inference()
 
 class BrothyClassifier(ImageClassifier):
