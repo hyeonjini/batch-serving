5cf78502abf0e5e03022b13b73dd65fb82518f63
hyeonjini <choihj94@huray.net>
2023-01-20 12:46:50 +0900

    [MOD] container

--- src/container.py
+++ src/container.py
@@ -1,6 +1,5 @@
 from dependency_injector import containers, providers
 
-from service import service
 from classifier import classifier
 from preprocess import preprocess
 
@@ -8,13 +7,13 @@ class Container(containers.DeclarativeContainer):
 
     config = providers.Configuration(yaml_files=["../config.yml"])
 
-    _preprocess = providers.Selector(
+    preprocess = providers.Selector(
         config.preprocess.type,
         cv=None,
         nlp=None,
     )
 
-    _classifier = providers.Selector(
+    classifier = providers.Selector(
         config.classifier.type,
         brothy=providers.Singleton(
             classifier.BrothyClassifier
@@ -28,17 +27,4 @@ class Container(containers.DeclarativeContainer):
         text=providers.Singleton(
             classifier.Textlassifier,
         ),
-    )
-    
-
-    challenge_service = providers.Selector(
-        config.service.type,
-        cv=providers.Singleton(
-            service.ComputerVisionChallengeService,
-            classifier=_classifier,
-        ),
-        nlp=providers.Singleton(
-            service.NLPChallengeService,
-            classifier=_classifier,
-        )
     )
