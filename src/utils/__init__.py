6335ef7014c0dd118607167ab5717174d7fa7806
hyeonjini <choihj94@gmail.com>
2023-01-30 10:02:51 +0900

    [ADD] load yaml file function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,24 @@
-from typing import Union
+from typing import Any, Union, Dict
 from PIL import Image
+
 import os
+import yaml
 import requests
+import addict
 
 
 def load_image(image: Union[str, "Image.Image"]) -> "Image.Image":
+    """_summary_
+
+    Args:
+        image (Union[str, Image.Image]): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        Image.Image: _description_
+    """
     
     if isinstance(image, str):
         if image.startswith("http://") or image.startswith("https://"):
@@ -21,3 +35,27 @@ def load_image(image: Union[str, "Image.Image"]) -> "Image.Image":
     
     image = image.convert("RGB")
     return image
+
+def load_yaml(cfg: Union[str, Dict[str, Any]], attr: bool=True) -> Dict[str, Any]:
+    """
+    
+
+    Args:
+        cfg (Union[str, Dict[str, Any]]): _description_
+
+    Returns:
+        Dict[str, Any]: _description_
+    """
+
+    config = None
+
+    if not os.path.exists(cfg):
+        raise FileNotFoundError(
+            "Incorret path used for config file. Should be in a local path."
+        )
+
+    with open(cfg) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    return addict.Dict(config) if attr else config
+    
